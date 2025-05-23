80633cf3a5cd47d01cad8f8fc8e7367106f2f978
Gourab Das <gdas37467@gmail.com>
2025-05-23 16:15:56 +0530

    fixed loader

--- app/chats/loading.tsx
+++ app/chats/loading.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-function loading() {
+export default function Spinner() {
   return (
-    <div>loading...</div>
-  )
+    <div className="flex justify-center items-center">
+      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+    </div>
+  );
 }
-
-export default loading
